3a4e6efc5196ab64fb41d990e58e3117c7acfd2b
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-07-28 18:53:20 +0900

    typo

--- serf-munin.pl
+++ serf-munin.pl
@@ -8,8 +8,8 @@ my ($hostname, $address, $role, $tags) = split(/\t/, <STDIN>);
 my $event = $ENV{SERF_EVENT};
 my $file  = "/etc/munin/conf.d/${hostname}.conf";
 
-my @tag_groups  = split(/,/, $tags);
-my ($group) =  map /^munin_group=(.+)/, @tags_group or die 'unknown tag';
+my @tags_group = split(/,/, $tags);
+my ($group) = map /^munin_group=(.+)/, @tags_group or die 'unknown tag';
 $group =~ s/^munin_group=//;
 chomp $group;
 
