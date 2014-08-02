d9bacf1263d3506eeae8bef79a91cb01e20f0804
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-08-02 16:10:21 +0900

    Multi-line for membership event data
    When members invoke events at once, serf send messages like following to stdin:
    
    one.local	127.0.0.1	one	role=one,datacenter=east
    two.local	127.0.0.1	two	role=two,datacenter=south
    three.local	127.0.0.1	three	role=three,datacenter=west

--- serf-munin.pl
+++ serf-munin.pl
@@ -4,37 +4,39 @@ use strict;
 use warnings;
 use Fcntl qw(:flock);
 
-my @member_fields = split("\t", <STDIN>);
-die "fields must include 4 elements" unless @member_fields == 4;
-my ($name, $address, undef, $tags) = @member_fields;
+while (<STDIN>) {
+    chomp;
+    my @member_fields = split("\t", $_);
+    die "fields must include 4 elements" unless @member_fields == 4;
+    my ($name, $address, undef, $tags) = @member_fields;
 
-my $event = $ENV{SERF_EVENT};
-my $file  = "/etc/munin/conf.d/${name}.conf";
+    my $event = $ENV{SERF_EVENT};
+    my $file  = "/etc/munin/conf.d/${name}.conf";
 
-chomp $tags;
-my @tags_group = split(/,/, $tags);
-my ($group) = map /^munin_group=(.+)/, @tags_group or die 'munin_group is nowhere';
-$group =~ s/^munin_group=//;
+    my @tags_group = split(/,/, $tags);
+    my ($group) = map /^munin_group=(.+)/, @tags_group or die 'munin_group is nowhere';
+    $group =~ s/^munin_group=//;
 
-if (
-    $event eq 'member-join' ||
-    $event eq 'member-update'
-) {
-    open my $fh, "> ${file}" or die $!;
-    flock($fh, LOCK_EX);
+    if (
+        $event eq 'member-join' ||
+        $event eq 'member-update'
+    ) {
+        open my $fh, "> ${file}" or die $!;
+        flock($fh, LOCK_EX);
 
-    print $fh <<CONF;
+        print $fh <<CONF;
 [${group};${name}]
     address ${address}
     use_node_name yes
 CONF
 
-    flock($fh, LOCK_UN);
-    close $fh;
-}
-elsif (
-    $event eq 'member-leave' ||
-    $event eq 'member-failed'
-) {
-    unlink $file;
+        flock($fh, LOCK_UN);
+        close $fh;
+    }
+    elsif (
+        $event eq 'member-leave' ||
+        $event eq 'member-failed'
+    ) {
+        unlink $file;
+    }
 }
